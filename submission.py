6804bc5c6ed3242296a60c28f58fc7f9216845f0
Rolando Gonzales Martinez <62504422+rogon666@users.noreply.github.com>
2024-04-30 11:52:59 +0200

    RF model

--- submission.py
+++ submission.py
@@ -1,99 +1,81 @@
+# -*- coding: utf-8 -*-
 """
-This is an example script to generate the outcome variable given the input dataset.
-
-This script should be modified to prepare your own submission that predicts 
-the outcome for the benchmark challenge by changing the clean_df and predict_outcomes function.
-
-The predict_outcomes function takes a Pandas data frame. The return value must
-be a data frame with two columns: nomem_encr and outcome. The nomem_encr column
-should contain the nomem_encr column from the input data frame. The outcome
-column should contain the predicted outcome for each nomem_encr. The outcome
-should be 0 (no child) or 1 (having a child).
-
-clean_df should be used to clean (preprocess) the data.
-
-run.py can be used to test your submission.
+--------------- Population Research Centre (RUG) ------------------------------
+Predicting Fertility data challenge (PreFer)
+-------------------------------------------------------------------------------
 """
-
 # List your libraries and modules here. Don't forget to update environment.yml!
+from IPython import get_ipython
+get_ipython().magic('reset -sf')
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
-import joblib
-
-
-def clean_df(df, background_df=None):
-    """
-    Preprocess the input dataframe to feed the model.
-    # If no cleaning is done (e.g. if all the cleaning is done in a pipeline) leave only the "return df" command
-
-    Parameters:
-    df (pd.DataFrame): The input dataframe containing the raw data (e.g., from PreFer_train_data.csv or PreFer_fake_data.csv).
-    background (pd.DataFrame): Optional input dataframe containing background data (e.g., from PreFer_train_background_data.csv or PreFer_fake_background_data.csv).
-
-    Returns:
-    pd.DataFrame: The cleaned dataframe with only the necessary columns and processed variables.
-    """
-
-    ## This script contains a bare minimum working example
-    # Create new variable with age
-    df["age"] = 2024 - df["birthyear_bg"]
-
-    # Imputing missing values in age with the mean
-    df["age"] = df["age"].fillna(df["age"].mean())
-
-    # Selecting variables for modelling
-    keepcols = [
-        "nomem_encr",  # ID variable required for predictions,
-        "age"          # newly created variable
-    ] 
-
-    # Keeping data with variables selected
-    df = df[keepcols]
-
+# ----------------------------------------------------------------------------
+def clean_df(dfy,dfx, background_df=None):
+    model_df = pd.merge(dfx,dfy, on="nomem_encr")
+    # Filter cases for whom the outcome is not available
+    consolidated_df = model_df[~model_df['new_child'].isna()]  
+    #%% One-hot encoding:
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['oplcat_2020'], prefix='eduencoded')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['cf20m024'], prefix='partner')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['cf20m454'], prefix='prevchild')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['ci20m006'], prefix='finsatisf')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['woning_2020'], prefix='dwelling')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['cr20m142'], prefix='religious15')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['cr20m143'], prefix='church')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['migration_background_bg'], prefix='migration')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['ch20m004'], prefix='health')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['cf20m029'], prefix='livwpartner')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['burgstat_2020'], prefix='civilstatus')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['cf20m180'], prefix='satisfpartner')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['cf20m128'], prefix='morechildren')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['cv20l125'], prefix='getmarried')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['cv20l126'], prefix='singleparent')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['cv20l112'], prefix='parcontribute')
+    # Impute missing values with the median of the column
+    new_columns = {
+    'brutohh_f_2020_imputed_median': consolidated_df['brutohh_f_2020'].fillna(consolidated_df['brutohh_f_2020'].median()),
+    'nettohh_f_2020_imputed_median': consolidated_df['nettohh_f_2020'].fillna(consolidated_df['nettohh_f_2020'].median())
+    }
+    consolidated_df = pd.concat([consolidated_df, pd.DataFrame(new_columns)], axis=1)
+    # Define the list of column names to select
+    selected_features = ['nomem_encr', 'new_child', 'birthyear_bg', 'age_bg', 'gender_bg',
+       'brutohh_f_2020_imputed_median', 'nettohh_f_2020_imputed_median',
+       'eduencoded_1.0', 'finsatisf_1.0', 'dwelling_1.0', 'religious15_1.0',
+       'church_1.0', 'migration_0.0', 'livwpartner_2015.0', 'civilstatus_1.0',
+       'satisfpartner_10.0', 'morechildren_1.0', 'getmarried_1.0',
+       'singleparent_1.0']
+    #%% Create a new DataFrame with the selected columns
+    df = consolidated_df[selected_features].copy()
     return df
+# Sources:
+source = 'Z:\\GitHub\\fertility-prediction-challenge\\'
+dfy = pd.read_csv(source + 'PreFer_fake_outcome.csv')
+dfx = pd.read_csv(source + 'PreFer_fake_data.csv')
+df_clean = clean_df(dfx,dfy,background_df=None)
 
 
-def predict_outcomes(df, background_df=None, model_path="model.joblib"):
-    """Generate predictions using the saved model and the input dataframe.
-
-    The predict_outcomes function accepts a Pandas DataFrame as an argument
-    and returns a new DataFrame with two columns: nomem_encr and
-    prediction. The nomem_encr column in the new DataFrame replicates the
-    corresponding column from the input DataFrame. The prediction
-    column contains predictions for each corresponding nomem_encr. Each
-    prediction is represented as a binary value: '0' indicates that the
-    individual did not have a child during 2021-2023, while '1' implies that
-    they did.
-
-    Parameters:
-    df (pd.DataFrame): The input dataframe for which predictions are to be made.
-    background_df (pd.DataFrame): The background dataframe for which predictions are to be made.
-    model_path (str): The path to the saved model file (which is the output of training.py).
-
-    Returns:
-    pd.DataFrame: A dataframe containing the identifiers and their corresponding predictions.
-    """
-
+def predict_outcomes(dfx, dfy, background_df=None, model_path="Z:\\GitHub\\fertility-prediction-challenge\\random_forest_model.joblib"):
+    df = clean_df(dfx, dfy, background_df=None)
+    df = df.drop(columns='new_child')
+    means = df.mean()
+    # Replace NaNs with the mean of their respective columns
+    df = df.fillna(means)
     ## This script contains a bare minimum working example
     if "nomem_encr" not in df.columns:
         print("The identifier variable 'nomem_encr' should be in the dataset")
-
     # Load the model
+    import joblib
     model = joblib.load(model_path)
-
-    # Preprocess the fake / holdout data
-    df = clean_df(df, background_df)
-
     # Exclude the variable nomem_encr if this variable is NOT in your model
     vars_without_id = df.columns[df.columns != 'nomem_encr']
-
     # Generate predictions from model, should be 0 (no child) or 1 (had child)
     predictions = model.predict(df[vars_without_id])
-
     # Output file should be DataFrame with two columns, nomem_encr and predictions
     df_predict = pd.DataFrame(
         {"nomem_encr": df["nomem_encr"], "prediction": predictions}
     )
-
     # Return only dataset with predictions and identifier
     return df_predict
+
+dfx = pd.read_csv(source + 'PreFer_fake_data.csv')
+dfy = pd.read_csv(source + 'PreFer_fake_outcome.csv')
+yholdout = predict_outcomes(dfx, dfy, background_df=None, model_path="Z:\\GitHub\\fertility-prediction-challenge\\random_forest_model.joblib")
--- training.py
+++ training.py
@@ -1,36 +1,101 @@
 """
-This is an example script to train your model given the (cleaned) input dataset.
-
-This script will not be run on the holdout data, 
-but the resulting model model.joblib will be applied to the holdout data.
-
-It is important to document your training steps here, including seed, 
-number of folds, model, et cetera
+--------------- Population Research Centre (RUG) ------------------------------
+Predicting Fertility data challenge (PreFer)
+-------------------------------------------------------------------------------
 """
-
-def train_save_model(cleaned_df, outcome_df):
-    """
-    Trains a model using the cleaned dataframe and saves the model to a file.
-
-    Parameters:
-    cleaned_df (pd.DataFrame): The cleaned data from clean_df function to be used for training the model.
-    outcome_df (pd.DataFrame): The data with the outcome variable (e.g., from PreFer_train_outcome.csv or PreFer_fake_outcome.csv).
-    """
-    
-    ## This script contains a bare minimum working example
-    random.seed(1) # not useful here because logistic regression deterministic
-    
-    # Combine cleaned_df and outcome_df
-    model_df = pd.merge(cleaned_df, outcome_df, on="nomem_encr")
-
-    # Filter cases for whom the outcome is not available
-    model_df = model_df[~model_df['new_child'].isna()]  
-    
-    # Logistic regression model
-    model = LogisticRegression()
-
-    # Fit the model
-    model.fit(model_df[['age']], model_df['new_child'])
-
-    # Save the model
-    joblib.dump(model, "model.joblib")
+from IPython import get_ipython
+get_ipython().magic('reset -sf')
+import joblib  # Ensure this import is at the top of your script
+import pandas as pd
+from sklearn.ensemble import RandomForestClassifier # Random forest
+from sklearn.model_selection import GridSearchCV
+from imblearn.over_sampling import ADASYN
+import numpy as np
+import random
+import os
+os.chdir('Z:\\GitHub\\fertility-prediction-challenge\\')
+#%% Loading data:
+source = 'C:\\2024\\_April_2024\\Prefer\\02_consolidated\\'
+consolidated_df = pd.read_csv(source + 'prefer_consolidated_db.csv', low_memory=False)
+#%% Cleaning function:
+def clean_df(consolidated_df, background_df=None):
+    # One-hot encoding:
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['oplcat_2020'], prefix='eduencoded')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['cf20m024'], prefix='partner')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['cf20m454'], prefix='prevchild')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['ci20m006'], prefix='finsatisf')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['woning_2020'], prefix='dwelling')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['cr20m142'], prefix='religious15')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['cr20m143'], prefix='church')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['migration_background_bg'], prefix='migration')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['ch20m004'], prefix='health')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['cf20m029'], prefix='livwpartner')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['burgstat_2020'], prefix='civilstatus')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['cf20m180'], prefix='satisfpartner')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['cf20m128'], prefix='morechildren')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['cv20l125'], prefix='getmarried')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['cv20l126'], prefix='singleparent')
+    consolidated_df = pd.get_dummies(consolidated_df, columns=['cv20l112'], prefix='parcontribute')
+    # Impute missing values with the median of the column
+    new_columns = {
+    'brutohh_f_2020_imputed_median': consolidated_df['brutohh_f_2020'].fillna(consolidated_df['brutohh_f_2020'].median()),
+    'nettohh_f_2020_imputed_median': consolidated_df['nettohh_f_2020'].fillna(consolidated_df['nettohh_f_2020'].median())
+    }
+    consolidated_df = pd.concat([consolidated_df, pd.DataFrame(new_columns)], axis=1)
+    # Seleting relevant variables:
+    # Define the list of column names to select
+    selected_features = ['nomem_encr',
+                         'new_child', # target
+                        'birthyear_bg',  
+                        'age_bg',
+                         'gender_bg',
+                        'brutohh_f_2020_imputed_median',
+                        'nettohh_f_2020_imputed_median',
+                        'eduencoded_1.0',  
+                         'finsatisf_1.0',
+                         'dwelling_1.0',
+                         'religious15_1.0',
+                         'church_1.0',
+                         'migration_0.0',
+                         'livwpartner_2015.0',
+                         'civilstatus_1.0',
+                         'satisfpartner_10.0',
+                         'morechildren_1.0',
+                         'getmarried_1.0',
+                         'singleparent_1.0']
+    # Create a new DataFrame with the selected columns
+    df = consolidated_df[selected_features].copy()
+    return df
+#%% --------------------------------------------------------------------------
+cleaned_df = clean_df(consolidated_df, background_df=None)
+cleaned_df.head(10)
+print(cleaned_df.columns)
+#%% --------------------------------------------------------------------------
+def train_save_model(cleaned_df):
+    # Seed for reproducibility
+    np.random.seed(666)  # Seed for NumPy operations
+    random.seed(666)  # Seed for Python random module
+    cross_validations = 10
+    y = cleaned_df['new_child'] # Target
+    X = cleaned_df.drop(columns=['new_child','nomem_encr'])
+    # oversampling with ADASYN
+    adasyn = ADASYN()
+    Xsam, ysam = adasyn.fit_resample(X, y)
+    # Define the parameter grid for RandomForestClassifier
+    param_grid = {
+        'n_estimators': [10, 50, 100],
+        'max_depth': [None, 10, 20],
+        'min_samples_split': [2, 5, 10],
+        'min_samples_leaf': [1, 2, 4]
+    }
+    clf = RandomForestClassifier()
+    grid_search = GridSearchCV(estimator=clf, param_grid=param_grid, cv=cross_validations, scoring='f1')
+    grid_search.fit(Xsam, ysam)
+    best_clf = grid_search.best_estimator_
+    best_clf.fit(Xsam, ysam)
+    predictions = best_clf.predict(Xsam)
+    joblib.dump(best_clf, 'random_forest_model.joblib')
+    return predictions
+#%% ------------------------------------------------------------------
+predictions = train_save_model(cleaned_df)
+print(predictions)
